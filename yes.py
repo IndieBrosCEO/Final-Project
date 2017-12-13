ba4af12cfe10091ae64fdd8a438a34c0189890e9
Descrates <descratesone@gmail.com>
2017-12-13 15:00:07 -0800

    Add files via upload

--- yes.py
+++ yes.py
@@ -1,35 +1,43 @@
-import pygame
 import sys
 import pygame.mixer
 import os
-from pygame.locals import *
+import random
 
 x = 400
 y = 300
 
+bulletlife = 0
+bulletlifecountd = 0
+
 bkg = (255, 211, 0)
 clr = (0, 0, 0)
 squ = (8, 11, 134)
-
-
 white = (255, 255, 255)
 blue = (0, 0, 255)
+red = (255, 0, 0)
 green = (0, 255, 0)
+yellow = (255, 255, 0)
 
 current_path = os.path.dirname(__file__)
 resource_path = os.path.join(current_path, 'resources')
-image_path = os.path.join(resource_path, 'images')  #
+image_path = os.path.join(resource_path, 'images') #
 
 pygame.init()
-size = (800, 600)
+size = (720, 720)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Tetrahedron")
 pygame.key.set_repeat(1, 1)
 font = pygame.font.SysFont("Stencil", 20)
 clock = pygame.time.Clock()
-pygame.mouse.set_visible(False)
+pygame.mouse.set_visible(True)
 
+bx = 100
+by = 100
+
+screen.fill(bkg)
+pygame.display.update()
 while True:
+    pygame.event.get()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -37,23 +45,55 @@ while True:
 
     keys_pressed = pygame.key.get_pressed()
 
-    if keys_pressed[pygame.K_LEFT]:
+    if keys_pressed[pygame.K_a]:
         x -= 5
-    if keys_pressed[pygame.K_RIGHT]:
+    if keys_pressed[pygame.K_d]:
         x += 5
-    if keys_pressed[pygame.K_UP]:
+    if keys_pressed[pygame.K_w]:
         y -= 5
-    if keys_pressed[pygame.K_DOWN]:
+    if keys_pressed[pygame.K_s]:
         y += 5
 
-    if x > 800:
-        x = 0
+    else:
+        bx = x
+        by = y
     if x < 0:
-        x = 800
-    if y > 600:
-        y = 0
+        x = 0
     if y < 0:
-        y = 600
+        y = 0
+    if x > 720:
+        x = 720
+    if y > 720:
+        y = 720
+
+    if pygame.mouse.get_pressed()[0] == 1:
+            bulletlife = 0
+            bulletlifecountd = 0
+            pygame.draw.circle(screen, red, (int(round(bx)), int(round(by))), 10)
+            pygame.display.update()
+            bx = x
+            by = y
+
+            while bulletlife == 0:
+
+                if pygame.mouse.get_pos()[0] < x + 25:
+                    bx = bx - 1
+                if pygame.mouse.get_pos()[1] < y + 25:
+                    by = by - 1
+                if pygame.mouse.get_pos()[0] > x - 25:
+                    bx = bx + 1
+                if pygame.mouse.get_pos()[1] > y - 25:
+                    by = by + 1
+                if bulletlifecountd == 100 or bulletlifecountd > 100:
+                    bulletlife = 1
+                pygame.draw.circle(screen, red, (int(round(bx)), int(round(by))), 10)
+                pygame.display.update()
+                bulletlifecountd += 1
+
+    def enemies_killed(count):
+        font = pygame.font.SysFont(None, 25)
+        text = font.render("Score: " + str(count), True, black)
+        gameDisplay.blit(text, (0, 0))
 
     screen.fill(bkg)
     text = font.render('(' + str(x) + ',' + str(y) + ')', True, clr)
@@ -62,18 +102,3 @@ while True:
     pygame.display.flip()
     clock.tick(60)
 
-    bullet = pygame.image.load(os.path.join(image_path, 'x.png'))
-    bullets = []
-    while 1:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-            elif event.type == MOUSEBUTTONDOWN:
-                shot.play()
-                bullets.append([event.pos[0] - 32, 500])
-
-        mx, my = pygame.mouse.get_pos()
-
-        for b in range(len(bullets)):
-            bullets[b][0] -= 10
